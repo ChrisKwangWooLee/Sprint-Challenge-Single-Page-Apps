37ccc0f465e1e3c6e249dd6fadea8271d4d4d2ff
Kwang Woo Lee <kl467@cornell.edu>
2019-12-21 01:34:50 +0900

    MVP

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+import Axios from "axios";
+import {useParams} from 'react-router-dom';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+function CharacterCard(props) {
+
+  let {id} = useParams();
+
+  const [characterData, setCharacterData] = useState({})
+
+  useEffect(() => {
+    Axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(response => {
+      setCharacterData(response.data);
+    })
+  },[characterData])
+
+
+
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src={characterData.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{characterData.name}</CardTitle>
+          <CardSubtitle>Species: {characterData.species}</CardSubtitle>
+          <CardText>Gender: {characterData.gender}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  )
 }
+
+export default CharacterCard;
